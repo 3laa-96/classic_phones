303d3fc8a025a3a9929ad0ac3ca0f4e139a23a69
3laa-96 <3laa.xvx@gmail.com>
2023-11-16 14:12:10 +0200

    1.2

--- incs/header.inc.php
+++ incs/header.inc.php
@@ -1,5 +1,4 @@
 <?php
-// session_start();
 require_once('config.php');
 ?>
 <!DOCTYPE html>
@@ -9,24 +8,25 @@ require_once('config.php');
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Library</title>
+  <title>Classic Phones</title>
   <link href="<?php echo BASEURL; ?>assets/css/bootstrap.min.css" rel="stylesheet" />
   <script src="<?php echo BASEURL; ?>assets/js/bootstrap.min.js"></script>
   <script src="<?php echo BASEURL; ?>assets/js/jquery.min.js"></script>
-  <link rel="stylesheet" href="assets/css/fa.css">
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="<?php echo BASEURL; ?>assets/images/favicon" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="<?php echo BASEURL; ?>assets/images/favicon" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest" />
+  <link rel="stylesheet" href="<?php echo BASEURL; ?>assets/css/fa.css" />
   <link rel="stylesheet" href="<?php echo BASEURL; ?>assets/css/style.css" />
-
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
   <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
   <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/css/intlTelInput.css">
-
   <script src="https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/intlTelInput.min.js"></script>
-
-
 </head>
 
 <body class="position-relative pb-5">
+  <!-- NAVIGATION BAR -->
   <nav class="navbar navbar-expand-md navbar-dark bg-dark">
     <div class="container-fluid">
       <span class="navbar-brand nokia" href="#">NOKIA</span>
